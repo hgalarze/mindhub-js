d1c9b058ea6ffd2abc14a32199f8365c4744caa9
hgalarze <hectorgalarze@gmail.com>
2023-03-04 16:31:58 +0100

    Added dynamic loading of event cards

---
+++ assets/js/pastEvents.js
@@ -0,0 +1,46 @@
+/*
+	Fill grid with cards events
+*/
+function fillGrid(events) {
+  try {
+    let eventCardsContainer = document.querySelector(".events-container");
+    const eventCardTemplate = document.querySelector("#event-card-template");
+    for (const event of events) {
+      // Build template
+      let eventCard = eventCardTemplate.content.cloneNode(true);
+      eventCardsContainer.append(eventCard);
+      eventCard = eventCardsContainer.lastElementChild;
+
+      // Set event card elements
+      const eventPhoto = eventCard.querySelector(".event-photo");
+      const eventTitle = eventCard.querySelector(".event-title");
+      const eventDescription = eventCard.querySelector(".event-description");
+      const eventPrice = eventCard.querySelector(".event-price");
+
+      // Set event properties
+      eventCard.dataset.id = event.id;
+      eventPhoto.src = event.image;
+      eventTitle.innerText = event.name;
+      eventDescription.innerText = event.description;
+      eventPrice.innerText = `$${event.price}`;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+/*
+	Get past event list given a date
+*/
+function getUpcomingEventsByDate(date, events) {
+  let pastEvents = [];
+  for (const event of events) {
+    if (event.date < date) {
+      pastEvents.push(event);
+    }
+  }
+  return pastEvents;
+}
+
+const pastEvents = getUpcomingEventsByDate(data.currentDate, data.events);
+fillGrid(pastEvents);
